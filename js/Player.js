f6115d3e38c02244dd16af71bc36de36d3103132
ShreyasS333 <shreyas333@gmail.com>
2020-10-09 18:39:47 +0530

    Rank

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = 0;
   }
 
   getCount(){
@@ -15,6 +16,18 @@ class Player {
   updateCount(count){
     database.ref('/').update({
       playerCount: count
+    });
+  }
+
+  getCarsAtEnd(){
+    database.ref('CarsAtEnd').on("value",(data)=>{
+      this.rank = data.val();
+    })
+  }
+
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      CarsAtEnd: rank 
     });
   }
 
